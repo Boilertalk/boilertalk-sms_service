35232c76385cb2b05d6de4c9fc1f22960ac41d38
Koray Koska <koray@koska.at>
2017-06-17 01:20:51 +0200

    Debug messages

--- sms_service.rb
+++ sms_service.rb
@@ -52,6 +52,9 @@ puts 'Setup complete...'
 host = ENV['REDIS_HOST']
 port = ENV['REDIS_PORT']
 password = ENV['REDIS_PASSWORD']
+puts "HOST: #{host}"
+puts "PORT: #{port}"
+puts "PASSWORD: #{password}"
 redis = SMSService::RedisHandler.new(Redis.new(host: host, port: port,
                                                password: password),
                                      REDIS_JOB_KEY)
